ce418a3989906379cd004994232d523279f89fae
Vlad <musteatavlad@yahoo.com>
2019-07-14 12:06:02 +0300

    maybeLastModificationForEdit

--- ProiectTeza/resources/lang/en/keyphrases.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'new_client' => 'New Client',
-    'total_rooms_booked' => 'Total rooms Booked',
-    'there_will_be' => 'There will be',
-	'new_booking' => 'New Booking',
-    'roomId'   	 => 'Room Id',
-];
-
--- ProiectTeza/resources/lang/ro/keyphrases.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'new_client' => 'Client Nou',
-    'total_rooms_booked' => 'Totalul de camere cazate',
-    'there_will_be' => 'Vor fi',
-	'new_booking' => 'Cazare Noua',
-    'roomId'   	 => 'Id Camera',
-];
-
