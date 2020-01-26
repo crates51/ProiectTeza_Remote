28a5dbd51a6d2ff5f41d45a0a42dcfcc44f9f920
Vlad <musteatavlad@yahoo.com>
2020-01-26 12:31:06 +0200

    change13

--- ProiectTeza/resources/js/components/vuepopper.vue
+++ ProiectTeza/resources/js/components/vuepopper.vue
@@ -11,7 +11,8 @@
           <i class='fas fa-envelope mr-1'></i>    {{client.Email}}<br>
           <i class='fas fa-male fa-lg mr-1'></i>  {{booking.Adults}}
           <i class='fas fa-child fa-lg mr-1 ml-4'></i>{{booking.Children}}<br>
-          {{booking.Checkin}} <i class='mx-3 fas fa-angle-double-right mt-3'></i>{{booking.Checkout}}
+          {{booking.Checkin}} <i class='mx-3 fas fa-angle-double-right mt-3'></i>{{booking.Checkout}}<br>
+          <i class="fas fa-id-card-alt fa-lg mr-2"></i>{{booking.bookingId}}
     </div>
     <span slot="reference">
       {{client.Last_Name}} {{client.First_Name}}
@@ -51,8 +52,8 @@
         cursor: pointer;
         background: #9799b5 ;
         border: 1px solid #565978;
-        width:300px;
-        height:150px;
+        width:310px;
+        height:155px;
         border-radius: 0.25rem;
         }
 </style>
